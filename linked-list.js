a2873bfbcf61a022813fb2bc16b18e4b9f534845
Rebecca Holt <rebecca.holt9991@gmail.com>
2016-12-12 16:58:40 -0700

    Start jquery. have event listener for enter button. doesn't fully work

---
+++ linked-list.js
@@ -0,0 +1,5 @@
+$('.enter-button').on('click', function(){
+  $('#bookmarked-website').text($('.website-title-input').val());
+  $('#bookmarked-url').text($('.website-url-input').val());
+  // $( "#bookmark" ).clone().appendTo( "#bookmark" );
+});
