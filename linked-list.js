442b19a7f4a61fdb28093be00684dcd161b16f1f
Rebecca Holt <rebecca.holt9991@gmail.com>
2016-12-13 16:29:05 -0700

    got clear read bookmarks to work.

--- linked-list.js
+++ linked-list.js
@@ -30,3 +30,60 @@ $('.right-side').on('click','#read-button', function () {
 $('.right-side').on('click', '#delete-button', function (){
   $(this).closest('section').remove();
 });
+
+$('.clear-button').on('click', function(){
+  $('.read').remove();
+});
+
+
+
+
+
+
+// $('.event-button').on('click', function() {
+// var $isValid = validateUrl($websiteUrl);
+// var $websiteTitle = $('.website-title-input').val();
+// var $websiteUrl = $('.website-url-input').val();
+//
+//
+//  if(!$isValid) {
+//    alert("Your URL is not valid. Try something like http://www.exampleurl.com");
+//    return;
+//  }
+//  $( ".right-side").append(`<section id="bookmark">
+//    <p id="bookmarked-website">${$websiteTitle}</p>
+//    <div></div>
+//    <a href="${$websiteUrl}" target="_blank" id='bookmarked-url'>${$websiteUrl}</a>
+//    <div></div>
+//    <button id="read-button" class="bookmark-buttons">Read</button>
+//    <button id="delete-button" class="bookmark-buttons">Delete</button>
+//  </section>`);
+//
+// function validateUrl($websiteUrl) {
+// var urlRegex = /^(http|https)?:\/\/[a-zA-Z0-9-\.]+\.[a-z]{2,4}/
+// return urlRegex.test($websiteUrl);
+// }
+// });
+//
+// // $('.website-url-input , .website-title-input').keyup(function(){
+// //   $('.enter-button').prop("disabled", false);
+// //   console.log('heyyy');
+// // });
+//
+// $('.right-side').on('click','#read-button', function () {
+//   $(this).closest('section').toggleClass('read');
+// });
+//
+// $('.right-side').on('click', '#delete-button', function (){
+//   $(this).closest('section').remove();
+// });
+//
+// $('.clear-button').on('click', function(){
+//   $('.read').remove();
+// });
+//
+//
+//
+// // $('input').on('keyup', function() {
+// //  $('.enter-button').attr('disabled',false);
+// // });
