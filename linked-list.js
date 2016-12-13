4c12ac1066e10f4a6b84a70b74b7884b3e5fcc9f
Rebecca Holt <rebecca.holt9991@gmail.com>
2016-12-12 17:25:45 -0700

    Fixed enter button! YAY! Thanks Robbie

--- linked-list.js
+++ linked-list.js
@@ -1,5 +1,5 @@
-$('.enter-button').on('click', function(){
+$('.enter-button').on('click', function() {
   $('#bookmarked-website').text($('.website-title-input').val());
   $('#bookmarked-url').text($('.website-url-input').val());
-  // $( "#bookmark" ).clone().appendTo( "#bookmark" );
+  $( "#bookmark").clone().appendTo( ".right-side" );
 });
