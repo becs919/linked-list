4e42996fc82296e5bbba173a4018dbbc5c3d4962
Rebecca Holt <rebecca.holt9991@gmail.com>
2016-12-12 17:39:32 -0700

    Fix the input fields and the button. Got phase one -bullet point one done! hooray

--- linked-list.js
+++ linked-list.js
@@ -1,5 +1,14 @@
 $('.enter-button').on('click', function() {
-  $('#bookmarked-website').text($('.website-title-input').val());
-  $('#bookmarked-url').text($('.website-url-input').val());
-  $( "#bookmark").clone().appendTo( ".right-side" );
+  var $websiteTitle = $('.website-title-input').val();
+  var $websiteUrl = $('.website-url-input').val();
+  // $('#bookmarked-website').text($websiteTitle);
+  // $('#bookmarked-url').text($websiteUrl);
+  $( ".right-side").append(`<section id="bookmark">
+    <p id="bookmarked-website">${$websiteTitle}</p>
+    <div></div>
+    <p id="bookmarked-url">${$websiteUrl}</p>
+    <div></div>
+    <button id="bookmark-buttons" class="read-button">Read</button>
+    <button id="bookmark-buttons" form=""class="delete-button">Delete</button>
+  </section>`);
 });
