d71b431a7170a573df922bfaa97561c5f553649d
Rebecca Holt <rebecca.holt9991@gmail.com>
2016-12-13 19:46:34 -0700

    Add count to page

--- linked-list.js
+++ linked-list.js
@@ -2,40 +2,56 @@ $('.enter-button').on('click', function() {
  var $websiteTitle = $('.website-title-input').val();
  var $websiteUrl = $('.website-url-input').val();
  var $isValid = validateUrl($websiteUrl);
- if(!$isValid) {
-   alert("Your URL is not valid. Try something like http://www.exampleurl.com");
-   return;
+if(!$isValid) {
+  alert("ERROR: Your URL is not valid try a formate like: http://www.exampleurl.com");
+  return;
  }
 
- $( ".right-side").append(`<section id="bookmark">
-   <p id="bookmarked-website">${$websiteTitle}</p>
-   <div></div>
-   <a href="${$websiteUrl}" target="_blank" id='bookmarked-url'>${$websiteUrl}</a>
-   <div></div>
-   <button id="read-button" class="bookmark-buttons">Read</button>
-   <button id="delete-button" class="bookmark-buttons">Delete</button>
- </section>`);
+$( ".right-side").append(`<section id="bookmark">
+  <p id="bookmarked-website">${$websiteTitle}</p>
+  <div></div>
+  <a href="${$websiteUrl}" target="_blank" id='bookmarked-url'>${$websiteUrl}</a>
+  <div></div>
+  <button id="read-button" class="bookmark-buttons">Read</button>
+  <button id="delete-button" class="bookmark-buttons">Delete</button>
+  </section>`);
 
+updateSectionCounter();
 
- function validateUrl($websiteUrl) {
- var urlRegex = /^(http|https)?:\/\/[a-zA-Z0-9-\.]+\.[a-z]{2,4}/
- return urlRegex.test($websiteUrl);
+function validateUrl($websiteUrl) {
+var urlRegex = /^(http|https)?:\/\/[a-zA-Z0-9-\.]+\.[a-z]{2,4}/
+return urlRegex.test($websiteUrl);
 }
 });
 
+
+
 $('.right-side').on('click','#read-button', function () {
   $(this).closest('section').toggleClass('read');
+  updateSectionCounter();
 });
 
 $('.right-side').on('click', '#delete-button', function (){
   $(this).closest('section').remove();
+  updateSectionCounter();
 });
 
 $('.clear-button').on('click', function(){
   $('.read').remove();
+  updateSectionCounter();
 });
 
 $('.website-url-input , .website-title-input').keyup(function(){
   $('.enter-button').prop("disabled", false);
   console.log('heyyy');
 });
+
+
+function updateSectionCounter() {
+  var bookmarkNum = $('.bookmarked-website').length;
+  var readNum = $('.read').length;
+  var unreadBookmarks = bookmarkNum - readNum;
+  $('.bookmark-counter').text(bookmarkNum);
+  $('.read-counter').text(readNum);
+  $('.unread-counter').text(unreadBookmarks);
+}
